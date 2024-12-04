743e5493408a8b5603a7c7b690337466f67a45b8
유인호 <yuiop1029@naver.com>
2024-12-04 10:52:25 +0900

    Initial Commit

---
+++ TestFirebaseCodable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestFirebaseCodable
+//
+//  Created by A_Mcflurry on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestFirebaseCodable/TestFirebaseCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFirebaseCodableApp.swift
+//  TestFirebaseCodable
+//
+//  Created by A_Mcflurry on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TestFirebaseCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
